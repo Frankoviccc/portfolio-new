e4a69ced49b7658708eb8133ca839ae7b1a60d4f
Frankoviccc <frank_struikk@hotmail.com>
2025-02-08 14:06:32 +0100

    Deleted import statement footer

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -6,16 +6,12 @@
     <main class="page">
         <slot />
     </main>
-
-    <FsFooter />
 </template>
 
 <script setup>
 import Logobar from '@/components/logobar/Logobar.vue';
-import FsFooter from "@/components/footer/FsFooter.vue";
 
 import { useStoryblok } from '@storyblok/vue';
-import Footer from "@/components/footer/FsFooter.vue";
 const story = await useStoryblok('config', { version: 'draft' });
 
 const logoBarItems = story.value.content.body[0];
